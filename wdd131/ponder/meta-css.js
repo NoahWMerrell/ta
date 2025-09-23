d2e559d86fabd3e5fd135d24c5b4b4b061a1782c
NoahWMerrell <168214327+NoahWMerrell@users.noreply.github.com>
2025-09-22 18:20:52 -0600

    Added meta-css JavaScript

---
+++ wdd131/ponder/meta-css.js
@@ -0,0 +1,39 @@
+const PI = 3.14;
+let radius = 3;
+// instead of using var use let
+
+let area = radius * radius * PI;
+
+console.log(area);
+
+radius = 20;
+
+area = radius * radius * PI;
+
+console.log(area);
+
+const one = 1;
+const two = '2';
+
+let result = one * two;
+console.log(result);
+
+result = one + Number(two);
+console.log(result);
+// the number changes it to number if it is in a string
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  // works just fine, course is global
+    console.log(student); // works just fine, it's being accessed within the block
+}
+console.log(course); // works fine, course is global
+// console.log(student); // does not work, can't access a block variable outside the block
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+    document.getElementById(codeValue).style.color = 'red';
+})
